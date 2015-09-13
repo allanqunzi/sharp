c78e5c6ebd379e4af9df6f492adcc0e9e1e92724
allanqunzi <allanqunzi@gmail.com>
2015-09-13 17:04:09 -0400

    revision for corner cases

--- src/pytrader/trader.py
+++ src/pytrader/trader.py
@@ -76,7 +76,7 @@ class BaseTrader(AbstractTrader):
     cores-1 - the number of worker processes for handling the bars
     """
     def __init__(self, wl = [], cores = 2, futurelist = True):
-        if self.check_values(wl, cores):
+        if self._check_values(wl, cores):
             self.wl = wl
             self.cores = cores
         self._dict = {}    # key: the process index; value: list of symbols
@@ -86,8 +86,8 @@ class BaseTrader(AbstractTrader):
         self._ps = []       # processes list
         self._stops = [mp.Event() for i in range(cores)] # events for IPC
         self._new_wl_dict = {} # newly being added/removed watchlist which will be distributed to each process
-        if futurelist:      # if promised the possibility to change wl in the future
-            self._locks = [mp.Lock() for i in range(cores)]
+        #if futurelist:      # if promised the possibility to change wl in the future
+        self._locks = [mp.Lock() for i in range(cores)]
 
     def trade(self):
         if self._sts:
@@ -96,6 +96,7 @@ class BaseTrader(AbstractTrader):
         for i in range(cores):
             if i == 0:
                 # process 0 only handles the queue self._evnts
+                self._pre_trade()
                 worker = mp.Process(target = self._evnts_handler, args = ())
                 self._ps.append(worker)
                 worker.daemon = True
@@ -111,12 +112,36 @@ class BaseTrader(AbstractTrader):
 
     def stop(self):
         if not self._sts:
+            logger.warn("trade() is not running, not meaningful to stop.")
             return True
         else:
             for i in range(self.cores):
                 self._stops[i].set()
                 self._ps[i].join()
+            self._post_trade()
+            self._sts = False
+            logger.info("trade() has stopped running.")
         return True
+
+    @abstractmethod
+    def _pre_trade(self):
+        raise NotImplementedError("Should implement _pre_trade().")
+
+    @abstractmethod
+    def _post_trade(self):
+        raise NotImplementedError("Should implement _post_trade().")
+
+    def set_futurelist(self, bl):
+        if self._sts:
+            logger.error("futurelist can not be set while trade() is running.")
+            return False
+        elif self.futurelist == bl:
+            logger.warn("futurelist is already", bl)
+            return True
+        else:
+            logger.info("futurelist is set to be", bl)
+            self.futurelist = bl;
+            return True
 
     def status(self):    # check if trade() is running.
         return self._sts
@@ -126,11 +151,11 @@ class BaseTrader(AbstractTrader):
 
     @abstractmethod
     def _evnts_handler(self):
-        raise NotImplementedError("Should implement _evnts_handler()")
+        raise NotImplementedError("Should implement _evnts_handler().")
 
     @abstractmethod
     def _feed_handler(self):
-        raise NotImplementedError("Should implement _feed_handler()")
+        raise NotImplementedError("Should implement _feed_handler().")
 
     def _spread_new_wl(self, newsbls):
         if not newsbls:
@@ -187,20 +212,20 @@ class BaseTrader(AbstractTrader):
             if sbl in v:
                 return k
 
-    def _check_values(wl, cores):
+    def _check_values(self, wl, cores):
         if not wl:
-            logger.error("empty watchlist.")
-            raise ValueError
+            #logger.error("empty watchlist.")
+            raise ValueError("empty watchlist.")
         for s in wl:
             if not s.isupper() or len(s) > 4 or len(s) < 1:
-                logger.error("watchlist should be uppercase, len(symbol) should be less than 5.")
-                raise ValueError
+                #logger.error("watchlist should be uppercase, len(symbol) should be less than 5.")
+                raise ValueError("watchlist should be uppercase, len(symbol) should be less than 5.")
         if cores < 2:
-            logger.error("the number of cores should be at least two.")
-            raise ValueError
+            #logger.error("the number of cores should be at least two.")
+            raise ValueError("the number of cores should be at least two.")
         if cores-1 > len(wl):
-            logger.error("cores-1 shouldn't be greater than the number of symbols in the watchlist.")
-            raise ValueError
+            #logger.error("cores-1 shouldn't be greater than the number of symbols in the watchlist.")
+            raise ValueError("cores-1 shouldn't be greater than the number of symbols in the watchlist.")
         return True
 
     def _spread_wl(self):
@@ -235,10 +260,11 @@ class LiveTrader(BaseTrader):
         self._transport = TTransport.TBufferedTransport(self._socket)
         self._protocol = TBinaryProtocol.TBinaryProtocol(self._transport)
         self._client = Sharp.Client(self._protocol)
-        self._transport.open()
+        #self._transport.open()
+        #self._client.removeZombieSymbols([])
 
-    def addToWatchList(symbols):
-        if futurelist:
+    def addToWatchList(self, symbols):
+        if self.futurelist and self._sts:
             if self._spread_new_wl(symbols):
                 for k, v in self._new_wl_dict:
                     with self._locks[k]:
@@ -248,12 +274,13 @@ class LiveTrader(BaseTrader):
             else:
                 return False
         else:
-            logger.error("futurelist = False, you have promised not to add new symbols.")
+            logger.error("futurelist = False, you have promised not to add new symbols; or trade() is not running.")
             return False
 
-    def removeFromWatchList(symbols):
-        if futurelist:
+    def removeFromWatchList(self, symbols):
+        if self.futurelist and self._sts:
             if self._remove_wl(symbols):
+                self._client.removeFromWatchList(self.wl)
                 for k, v in self._new_wl_dict:
                     with self._locks[k]:
                         for item in v:
@@ -262,11 +289,28 @@ class LiveTrader(BaseTrader):
             else:
                 return False
         else:
-            logger.error("futurelist = False, you have promised not to remove new symbols.")
+            logger.error("futurelist = False, you have promised not to remove new symbols; or trade() is not running.")
             return False
 
+    def _pre_trade(self):
+        pass
+
+    def _post_trade(self):
+        pass
+
+    def _evnts_handler(self):
+        pass
+
+    def _feed_handler(self):
+        pass
 
 class TestTrader(BaseTrader):
     """docstring for TestTrader"""
     def __init__(self, wl=[], cores = 2, futurelist = True):
         super(TestTrader, self).__init__(wl, cores, futurelist)
+
+
+if __name__ == '__main__':
+
+    watchlist = ['AAPL', 'AMZN']
+    trader = LiveTrader(watchlist)
